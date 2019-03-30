6e59329116dc6871ebf3f87e2f8b41891805ed2e
2019jrhee <2019jrhee@dwight.edu>
2019-03-29 23:06:39 -0400

    added views and templates for posts

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,18 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
+# post model: what a post will be comprised of
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     published_date = models.DateTimeField(blank=True, null=True)
 
+# publish function that will show published date and save the post
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
+# showing the title of the post
     def __str__(self):
         return self.title
